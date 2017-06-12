d56fd4968efd5a74126fb211dc25b1abae43fae2
Mehran Hassani <m_assani@encs.concordia.ca>
2017-06-12 11:45:15 -0400

    minor updates and improvements to home and contact

--- js/main.js
+++ js/main.js
@@ -88,8 +88,8 @@ jQuery(function($) {'use strict';
 
 		map = new GMaps({
 			el: '#gmap',
-			lat: 43.04446,
-			lng: -76.130791,
+			lat: 45.495706,
+			lng: -73.5781895,
 			scrollwheel:false,
 			zoom: 16,
 			zoomControl : false,
@@ -101,8 +101,8 @@ jQuery(function($) {'use strict';
 		});
 
 		map.addMarker({
-			lat: 43.04446,
-			lng: -76.130791,
+			lat: 45.495706,
+			lng: -73.5781895,
 			animation: google.maps.Animation.DROP,
 			verticalAlign: 'bottom',
 			horizontalAlign: 'center',
